4031a3be848b7f35546bcf4d6368fef14168f361
Bar Ben Waiss <psnoob510@gmail.com>
2023-10-20 14:30:26 +0300

    made button animations

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,15 +3,28 @@ import './nav.css'
 import logo from '../assets/logo_2.png'
 
 function Nav() {
+  const scrollToHome = () => {
+    window.scrollTo({top : 0, behavior: 'smooth'}
+    )
+  }
+
+  const scrollToProjects = () => {
+    window.scrollTo({top: window.innerHeight, behavior: 'smooth'})
+  }
+
+  const scrollToAbout = () => {
+    window.scrollTo({top: document.body.scrollHeight, behavior: 'smooth'})
+  }
+
   return (
     <nav>
         <div className="logo">
             <img src={logo} alt="logo" />
         </div>
         <div className="buttons">
-            <a href="">Home</a>
-            <a href="">Projects</a>
-            <a href="">About</a>
+            <div onClick={scrollToHome}>Home</div>
+            <div onClick={scrollToProjects}>Projects</div>
+            <div onClick={scrollToAbout}>About</div>
         </div>
         <div className="cv">
             <a href=""><i class="fa-regular fa-file-lines fa-lg"></i></a>
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './about.css'
 import Footer from '../components/Footer'
 
