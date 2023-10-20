7a2214807d87d81d0b88f2b4286211c929751a43
Bar Ben Waiss <psnoob510@gmail.com>
2023-10-20 13:25:41 +0300

    finished projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './projects.css'
 import chess from '../assets/chess.png'
+import tic_tac_toe from '../assets/tic_tac_toe.png'
+import personal_website from '../assets/personal_website.png'
+import lambo from '../assets/lambo_original.jpg'
+import ascii from '../assets/lmabo_processed.png'
 
 function Projects() {
   return (
@@ -20,6 +24,44 @@ function Projects() {
               <a href="">Source code</a>
             </div>
           </div>
+          <div className="showcase" id='tic_tac_toe'>
+            <div className="project_headline">Tic Tac Toe</div>
+            <div className="project_content">
+              <p>Developed in JavaScript; This is a simple Tic-Tac-Toe game yet with a not so simple AI to defeat.</p>
+              <img src={tic_tac_toe} alt="tic_tac_toe" />  
+            </div>
+            <div className="project_btns">
+              <a href="">Source code</a>
+              <a href="">Play game</a>
+            </div>
+          </div>
+      </div>
+      <div className="section" id='websites'>
+        <div className="section_headline">Websites</div>
+        <div className="showcase" id='personal_website'>
+          <div className="project_headline">Personal Webites</div>
+          <div className="project_content">
+            <p>Developed in React; The very website you are browsing on right now.</p>
+            <img src={personal_website} alt="personal_website" />  
+          </div>
+          <div className="project_btns">
+            <a href="">Source code</a>
+          </div>
+        </div>
+      </div>
+      <div className="section" id='art'>
+        <div className="section_headline">Art</div>
+        <div className="showcase" id='ascii'>
+          <div className="project_headline">ASCII Art</div>
+          <div className="project_content">
+            <p>Developed in Python using PIL and OpenCV libraries; Transforms images/videos to ASCII characters.</p>
+            <img src={lambo} alt="lambo" />  
+            <img src={ascii} alt="ascii_lambo" />
+          </div>
+          <div className="project_btns">
+            <a href="">Source code</a>
+          </div>
+        </div>
       </div>
     </div>
   )
